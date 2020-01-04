abb6e223ee40e96cde17a2d8100890b50c5cd41e
Niklas Merz <niklasmerz@linux.com>
2020-01-05 00:40:18 +0100

    Improve Landing Page

--- src/assets/sandbox/script.js
+++ src/assets/sandbox/script.js
@@ -1,7 +1,11 @@
 /** @type {Pecuniator} */
 const pecuniator = api;
 
-//Your data is available with the `pecuniator` variable. Use autocomplete for assitance
+/**
+Your data is available with the `pecuniator` variable. Use autocomplete for assistance
+You can find documentaion for all available objects here: https://docs.pecuniator.com/modules/_interface_.html
+**/
+
 const acc = pecuniator.accounts[0];
 
 this.console.info('Script', acc.currency);
