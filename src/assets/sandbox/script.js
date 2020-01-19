3a12ed21f06ac12fac68071ce82f42bad4cb647a
Niklas Merz <niklasmerz@linux.com>
2020-01-19 20:08:37 +0100

    New example script

--- src/assets/sandbox/script.js
+++ src/assets/sandbox/script.js
@@ -1,20 +1,71 @@
 /** @type {Pecuniator} */
 const pecuniator = api;
-
 /**
 Your data is available with the `pecuniator` variable. Use autocomplete for assistance
 You can find documentaion for all available objects here: https://docs.pecuniator.com/modules/_interface_.html
 **/
-
 const acc = pecuniator.accounts[0];
-
 this.console.info('Script', acc.currency);
 const firstTransaction = pecuniator.entries[0];
 
+const categories = { //Add you categories here with id, title and search query.
+    saving: {
+        remittanceInformation: ["DEPOT"],
+        amount: 0,
+        title: "Savings"    
+    },
+    fuel: {
+        creditorName: ["Tank"],
+        amount: 0,
+        title: "Tank"   
+    },
+    insu: {
+        remittanceInformation: ["Versicherung"],
+        amount: 0,
+        title: "Insurance"   
+    },
+    online: {
+        creditorName: ["Paypal"],
+        amount: 0,
+        title: "Online"   
+    },
+    cell: {
+        creditorName: ["Drillisch", "Telekom"],
+        amount: 0,
+        title: "Cellphone"   
+    },
+};
 if (!firstTransaction) {
     this.postMessage({ error: 'No transaction' })
 } else {
-    this.postMessage({ text: firstTransaction.amount + firstTransaction.currency + ' from ' + firstTransaction.creditorIBAN });
+    for (const entry of pecuniator.entries) {
+        entry.found = []
+        checkCategories(entry);         
+    }
+    console.log(categories)
+    for(const name in categories) {
+        const cat = categories[name];
+        this.postMessage({ textAppend: `|| ${cat.title} - ${cat.amount}€ ||` });
+    }
 }
-
-this.postMessage({ textAppend: '.' });
+/** @type {PecuniatorEntry} */
+function checkCategories(tmp) {
+    /** @type {PecuniatorEntry} */
+    const entry = tmp;
+    for(const catname in categories) {
+        const cat = categories[catname];
+        for(const check in cat) {
+            if(check !== "amount" && check !== "title" && typeof entry[check] !== "undefined") {
+                if (typeof entry[check] === "string"){
+                    entry[check] = [entry[check]];
+                }
+                for (const test of cat[check]) {
+                    if (!entry.found.includes(catname) && entry[check].toString().toLowerCase().includes(test.toLowerCase())) {
+                        cat.amount += new Number(entry.amount);
+                        entry.found.push(catname);
+                    }
+                }
+            }
+        }
+    }
+}
